5cc134a3794dc1b58a04c83e3d6d3f61b011f6d3
Christian Sheridan <christiansheridan@outlook.com>
2023-01-15 03:13:16 -0600

    Small cleanup and fixes

--- src/App.fs
+++ src/App.fs
@@ -2,40 +2,48 @@ module App
 
 open Feliz
 open Feliz.DaisyUI
-open Feliz.UseElmish
 open Elmish
+open Feliz.UseElmish
+open Browser.Dom
+open Fable.Core.JsInterop
 
-type State = { count: int }
+module Counter =
+  type State = { count: int }
 
-type Msg =
-  | Increment
-  | Decrement
+  type Msg =
+    | Increment
+    | Decrement
 
-let update (msg: Msg) (state: State) : State * Cmd<Msg> =
-  match msg with
-  | Increment -> { state with count = state.count + 1 }, Cmd.none
-  | Decrement -> { state with count = state.count - 1 }, Cmd.none
-  
-let init () = { count = 0 }, Cmd.none
+  let update (msg: Msg) (state: State) : State * Cmd<Msg> =
+    match msg with
+    | Increment -> { state with count = state.count + 1 }, Cmd.none
+    | Decrement -> { state with count = state.count - 1 }, Cmd.none
+    
+  let init () = { count = 0 }, Cmd.none
 
-let clickableButton isPrimary (text: string) onClick =
-  Daisy.button.button [
-    button.outline
-    if isPrimary then button.primary
-    button.lg
-    prop.text text
-    prop.onClick (onClick)
-  ]
-
-[<ReactComponent>]
-let Component () =
-  let state, dispatch = React.useElmish(init, update, [||])
-  
-  Html.div [
-    Html.h1 (state.count |> string)
-
-    Html.div [
-      clickableButton true "Increment" (fun _ -> dispatch Increment)
-      clickableButton true "Increment" (fun _ -> dispatch Decrement)
+  let clickableButton isPrimary (text: string) onClick =
+    Daisy.button.button [
+      button.outline
+      if isPrimary then button.primary
+      button.lg
+      prop.text text
+      prop.onClick (onClick)
     ]
-  ]
+
+  [<ReactComponent>]
+  let Component () =
+    let state, dispatch = React.useElmish(init, update)
+    
+    Html.div [
+      Html.h1 (state.count |> string)
+
+      Html.div [
+        clickableButton true "Increment" (fun _ -> dispatch Increment)
+        clickableButton true "Increment" (fun _ -> dispatch Decrement)
+      ]
+    ]
+  
+importSideEffects "./index.css"
+
+let root = ReactDOM.createRoot(document.getElementById "root")
+root.render(Counter.Component())
